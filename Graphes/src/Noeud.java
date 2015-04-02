8a10c4131ee2cea95a5a16e94542cd7020df235c
Spazzou <dropbox_wass@hotmail.fr>
2015-04-02 17:14:01 +0200

    Classe Noeud

--- Graphes/src/Noeud.java
+++ Graphes/src/Noeud.java
@@ -1,9 +1,27 @@
+import java.awt.List;
 import java.util.ArrayList;
 
 
 public class Noeud {
+	
 	int id;
-	ArrayList<Arete> liens;
+	ArrayList<Arete> liens ;
+	
+	public Noeud() {
+		liens = new ArrayList<Arete>();
+	}
+	public void addArete(Arete toAdd){
+		liens.add(toAdd);
+	}
+	public ArrayList<Arete> getSuccesseurs(){
+		ArrayList<Arete> toReturn = new ArrayList<Arete>();
+		for(int i=0;i<liens.size();i++){
+			if(liens.get(i).getNoeudDebut()==this){
+				toReturn.add(liens.get(i));
+			}
+		}
+		return toReturn;
+	}
 	public int getId() {
 		return id;
 	}
@@ -15,5 +33,16 @@ public class Noeud {
 	}
 	public void setLiens(ArrayList<Arete> liens) {
 		this.liens = liens;
+	}
+	public static void main(String[] args){
+		Noeud t1=new Noeud();
+		System.out.println(t1.getSuccesseurs());
+		Noeud t2=new Noeud();
+		Arete a1=new Arete(t1,t2);
+		System.out.println(t1.getSuccesseurs().get(0).getNoeudFin());
+	}
+	@Override
+	public String toString() {
+		return "Noeud [id=" + id + ", liens=" + liens + "]";
 	}
 }
