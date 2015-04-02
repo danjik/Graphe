a3721c9ee719c7aff457588a79871f21d90324ef
Jordan <jtaillefer02@gmail.com>
2015-04-02 16:43:27 +0200

    Ajout arete

---
+++ Graphes/src/Arete.java
@@ -0,0 +1,37 @@
+
+public class Arete {
+	private Noeud noeudFin;
+	private Noeud noeudDebut;
+	
+	public Arete(Noeud noeudDebut,Noeud noeudFin){
+		
+	}
+	
+	/**
+	 * @return the noeudDebut
+	 */
+	public Noeud getNoeudDebut() {
+		return noeudDebut;
+	}
+	/**
+	 * @param noeudDebut the noeudDebut to set
+	 */
+	public void setNoeudDebut(Noeud noeudDebut) {
+		this.noeudDebut = noeudDebut;
+	}
+	
+	/**
+	 * @return the noeudFin
+	 */
+	public Noeud getNoeudFin() {
+		return noeudFin;
+	}
+	/**
+	 * @param noeudFin the noeudFin to set
+	 */
+	public void setNoeudFin(Noeud noeudFin) {
+		this.noeudFin = noeudFin;
+	}
+	
+	
+}
